74bef70cd10db04da3c013274910d8af11ad6f3e
Umaima Zainab <umaimazainab429@gmail.com>
2026-09-11 13:11:25 +0500

    Added loop practice questions

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div className="h-screen bg-black text-white ">
       <div className="flex flex-col lg:flex-row ">
-        <div className='flex flex-col  w-full lg:w-1/2 mr-1 pl-8' >
+        <div className='flex flex-col  w-full lg:w-1/2 mr-1 px-6 lg:px-0 lg:pl-8' >
           <h1 className="text-4xl font-bold mb-10 mt-5">
             Add Notes <br />
           </h1>
@@ -16,12 +16,12 @@ const App = () => {
             <input
               type="text"
               placeholder="Enter Notes Heading"
-              className="border border-white w-full lg:w-[90%] bg-gray-900 text-white p-3 rounded"
+              className="border border-white w-full lg:w-[90%]  text-white p-3 rounded"
             />
 
             <textarea
               placeholder="Write details"
-              className="border border-white w-full lg:w-[90%] bg-gray-900 text-white p-3 h-50 rounded"
+              className="border border-white w-full lg:w-[90%]  text-white p-3 h-50 rounded"
             />
 
             <button
@@ -34,19 +34,19 @@ const App = () => {
           </form>
         </div>
         {/* Right Side - Recent Notes */}
-        <div className="w-full lg:w-3/5 h-screen border-l-2 border-white pl-5">
+        <div className="w-full lg:w-1/2 h-screen lg:border-l-2 border-white pl-8">
 
-          <h2 className="text-3xl font-bold mb-5 mt-5 ">
+          <h2 className="text-4xl font-bold mb-5 mt-5 ">
             Recent Notes
           </h2>
 
           <div className="flex flex-wrap gap-5 overflow-auto">
 
-            <div className="h-55 w-50 rounded-2xl bg-white"></div>
+            <div className="h-55 w-68 lg:w-45 rounded-2xl bg-white"></div>
 
-            <div className="h-55 w-50 rounded-2xl bg-white"></div>
-            <div className="h-55 w-50 rounded-2xl bg-white"></div>
-            <div className="h-55 w-50 rounded-2xl bg-white"></div>
+            <div className="h-55 w-68 lg:w-45  rounded-2xl bg-white"></div>
+            <div className="h-55 w-68 lg:w-45  rounded-2xl bg-white"></div>
+            <div className="h-55 w-68 lg:w-45  rounded-2xl bg-white"></div>
             
 
           </div>
